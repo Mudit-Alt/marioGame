4db65f9a7d1dbda580e018a1e0c3da32303236d3
Mudit_Chandak7C <96127224+Mudit-Alt@users.noreply.github.com>
2023-05-26 17:32:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	mario_jump = loadSound("jump.wav")
+	mario_collect = loadSound("coin.wav")
+	mario_gameover = loadSound("gameover.wav")
+	mario_kick = loadSound("kick.wav")
+	mario_die = loadSound("mariodie.wav")
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240, 336);
+	canvas.parent('canvas')
+
+	instializeInSetup(mario);
+
+	video = createCapture(800,400);
+	video.size(800,400);
+	video.parent('console')
+
+	poseNet = ml5.poseNet(video, modalLoaded);
+	poseNet.on('pose', gotPoses);
+}
+
+function modalLoaded(){
+	console.log('Model Loaded!!!')
+}
+function gotPoses(results){
+if(results.length > 0){
+	noseX = results[0].pose.nose.x;
+	noseY = results[0].pose.nose.y;
+}
+
+}
+function draw() {
+	game()
+}
